837ab27b4c426fa675b617f57acce932e239bd1a
Justin Miller <jayman1294@gmail.com>
2019-10-20 04:23:57 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  birthday_kids.ecah do |kids_name, age|
+  birthday_kids.each do |kids_name, age|
     if age > 9
       puts "You are too old for this"
     else
